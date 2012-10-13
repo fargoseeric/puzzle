79588767ccc812b1e04aec34e5211ca3202f7cee
Eric Fargose <eric@kodeplay.com>
2012-10-13 12:19:01 +0530

    titanic puzzle

---
+++ titanic.php
@@ -0,0 +1,50 @@
+<?php
+
+$input = 17;
+
+if($input <= 1000000000) {
+
+    $input_array = array();
+    for($i=1; $i<=$input; $i++){
+        $input_array[$i] = $i; 
+    } 
+        
+    if(count($input_array) > 1) {
+        $result = removeOdd($input_array);
+               
+        for($j = 0; $j < count($input_array); $j++){
+            $result = removeOdd($result); 
+            if(count($result) <= 1){
+                echo $result[1];
+            }
+                    
+        }
+    
+    } else {
+        echo "confusion";
+    }
+    
+    
+}
+
+function removeOdd($input){
+
+    foreach($input as $key => $value) {
+        if($key&1) {
+            unset($input[$key]);
+        }
+    } 
+    
+    $i = 1;
+    foreach($input as $key => $val) {
+        $result[$i] = $val;     
+        $i++;
+    }
+    
+    return($result);
+}
+
+
+
+
+?>
